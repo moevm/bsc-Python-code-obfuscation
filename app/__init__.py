340bb2fe0f26e5493a2af84a6f58b2a258e73112
AleksandrRybin <vfibyrf007@gmail.com>
2020-02-08 23:53:06 +0300

    add DBEngine class for work with database

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,8 @@ app.config['SECRET_KEY'] = os.urandom(16)
 app.config['DB_URL'] = os.environ['PYTHON_CODE_OBFUSCATION_MONGODB_URL']
 app.config['DB_NAME'] = os.environ['PYTHON_CODE_OBFUSCATION_MONGODB_DB_NAME']
 
+from app.database import db_engine
+
+app.db_engine = db_engine.DBEngine(app.config['DB_URL'], app.config['DB_NAME'])
+
 from app.routes import routes
---
+++ app/database/db_engine.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+import pymongo
+
+
+class DBEngine:
+    COLLECTION_NAME = 'source_codes'
+
+    def __init__(self, mongo_url, db_name):
+        self.mongo_url = mongo_url
+        self.db_name = db_name
+
+        self.mongo = pymongo.MongoClient(self.mongo_url)      # MongoDB instance
+        self.db = self.mongo[self.db_name]                    # Database
+        self.collection = self.db[DBEngine.COLLECTION_NAME]   # Collection
+
+        self.collection.create_index('file_name')
+        self.collection.create_index('upload_date')
+        self.collection.create_index([
+            ('file_name', pymongo.ASCENDING),
+            ('upload_date', pymongo.DESCENDING)
+        ])
+      
+    
+    def upload(self, file_name, code, tags):
+        insert_result = self.collection.insert_one({
+            'file_name': file_name,
+            'code': code,
+            'tags': tags,
+            'length': len(code),
+            'upload_date': datetime.utcnow()
+        })
+
+        return insert_result.inserted_id
+
+
+    def get_tags(self):
+        return self.collection.distinct('tags')
+
+
+    def get_files_by_any_tags(self, tags):
+        return self.collection.find({
+            'tags': {
+                '$elemMatch': {
+                    '$in': tags
+                }
+            }
+        })
+
+
+    def get_files_by_tags(self, tags):
+        return self.collection.find({
+            'tags': {
+                '$all': tags
+            }
+        })
+
+
+    def get_file_by_id(self, id):
+        return self.collection.find_one({
+            '_id': id
+        })
