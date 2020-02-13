2414d0656cc1abd73af7e0bb85068d21e0f49d59
AleksandrRybin <vfibyrf007@gmail.com>
2020-02-13 10:18:04 +0300

    add obfuscate function prototype

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -66,6 +66,26 @@ def obfuscate_settings():
     else:
         flash('отсутствует параметр id', 'error')
         return redirect(url_for('index_page'))
+
+
+@app.route('/obfuscate', methods=['POST'])
+def obfuscate():
+    file_id = request.args.get('id', None)
+
+    if file_id is not None:
+        file = app.db_engine.get_file_by_id(file_id)
+
+        if file is not None:
+            # TODO obfuscation here
+            return redirect(url_for('view_code', id=file_id))
+        else:
+            flash(f'неверный параметр id(={file_id})', 'error')
+            return redirect(url_for('index_page'))    
+    else:
+        flash('отсутствует параметр id', 'error')
+        return redirect(url_for('index_page'))
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html', msg='Такой страницы не существует')
