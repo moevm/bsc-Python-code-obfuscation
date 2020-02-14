b1053900e487794a94ee5cb1a71341ac653a778e
AleksandrRybin <vfibyrf007@gmail.com>
2020-02-14 20:09:25 +0300

    add separate views for upload text or file with source code

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,89 +1,68 @@
 import flask
 
 from app import app
-
+from app.database.storage_type import StorageType
 
 @app.route('/')
 def index_page():
     return flask.render_template('index.html')
 
 
-@app.route('/view_code')
-def view_code():
-    file_id = flask.request.args.get('id', None)
+@app.route('/upload_text', methods=['GET', 'POST'])
+def upload_text():
+    request_method = flask.request.method
 
-    if file_id is not None:
-        file = app.db_engine.get_file_by_id(file_id)
+    if request_method == 'GET':
+        return flask.render_template('upload_text.html')
+    elif request_method == 'POST':
+        source_code = flask.request.form['source_code']
 
-        if file is not None:
-            return flask.render_template('view_code.html', file=file)
+        tags = flask.request.form['tags'].split(',')
+
+        save_to_db = True if flask.request.form['save_to_db'] == 'on' else False
+        storage_type = StorageType.DATABASE if save_to_db else StorageType.TEMPORARY
+
+        if storage_type == StorageType.DATABASE:
+            inserted_id = app.db_engine.upload(None, source_code, tags)
         else:
-            flask.flash(f'неверный параметр id(={file_id})', 'error')
-            return flask.redirect(flask.url_for('index_page'))    
+            inserted_id = app.db_engine.generate_id()
+            serialized_file = app.db_engine.serialize_file(None, source_code, tags)
+            app.tmp_storage[str(inserted_id)] = serialized_file
+
+        return flask.redirect(flask.url_for('obfuscate_settings', storage_type=storage_type, id=inserted_id))
     else:
-        flask.flash('отсутствует параметр id', 'error')
-        return flask.redirect(flask.url_for('index_page'))
+        flask.abort(400)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if flask.request.method == 'GET':
-        return flask.render_template('upload.html')
-    else:
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    request_method = flask.request.method
+
+    if request_method == 'GET':
+        return flask.render_template('upload_text.html')
+    elif request_method == 'POST':
         file = flask.request.files['source_code']
         
-        filename = file.filename
+        file_name = file.filename
         
         source_code_bytes = file.read()
         source_code = str(source_code_bytes, 'utf-8')
 
-        tags_str = flask.request.form['tags']
-
-        if len(tags_str) == 0:
-            tags = []
-        else:
-            tags = tags_str.split(',')
+        tags = flask.request.form['tags'].split(',')
 
         save_to_db = True if flask.request.form['save_to_db'] == 'on' else False
+        storage_type = StorageType.DATABASE if save_to_db else StorageType.TEMPORARY
 
-        inserted_id = app.db_engine.upload(filename, source_code, tags)
-
-        return flask.redirect(flask.url_for('obfuscate_settings', id=str(inserted_id)))
-
-
-@app.route('/obfuscate_settings')
-def obfuscate_settings():
-    file_id = flask.request.args.get('id', None)
-
-    if file_id is not None:
-        file = app.db_engine.get_file_by_id(file_id)
-
-        if file is not None:
-            return flask.render_template('obfuscate_settings.html', file=file, file_id=file_id)
+        if storage_type == StorageType.DATABASE:
+            inserted_id = app.db_engine.upload(file_name, source_code, tags)
         else:
-            flask.flash(f'неверный параметр id(={file_id})', 'error')
-            return flask.redirect(flask.url_for('index_page'))    
+            inserted_id = app.db_engine.generate_id()
+            serialized_file = app.db_engine.serialize_file(file_name, source_code, tags)
+            app.tmp_storage[str(inserted_id)] = serialized_file
+
+        return flask.redirect(flask.url_for('obfuscate_settings', storage_type=storage_type, id=inserted_id))
     else:
-        flask.flash('отсутствует параметр id', 'error')
-        return flask.redirect(flask.url_for('index_page'))
-
-
-@app.route('/obfuscate', methods=['POST'])
-def obfuscate():
-    file_id = flask.request.args.get('id', None)
-
-    if file_id is not None:
-        file = app.db_engine.get_file_by_id(file_id)
-
-        if file is not None:
-            # TODO obfuscation here
-            return flask.redirect(flask.url_for('view_code', id=file_id))
-        else:
-            flask.flash(f'неверный параметр id(={file_id})', 'error')
-            return flask.redirect(flask.url_for('index_page'))    
-    else:
-        flask.flash('отсутствует параметр id', 'error')
-        return flask.redirect(flask.url_for('index_page'))
+        flask.abort(400)
 
 
 @app.errorhandler(404)
