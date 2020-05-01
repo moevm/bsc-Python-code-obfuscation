4ebe899a64a5300c9a7b999c72601e01c04b2d37
AleksandrRybin <vfibyrf007@gmail.com>
2020-05-01 19:29:22 +0300

    add check if element found

--- app/static/scripts/accordion.js
+++ app/static/scripts/accordion.js
@@ -2,14 +2,16 @@ function w3Show(id, color) {
     let element = $(`#${id}`);
     let showClass = 'w3-show';
 
-    if (element.hasClass(showClass)) {
-        element.removeClass(showClass);
-    } else {
-        element.addClass(showClass);
-    }
-
-    if (color) {
-        w3SwitchColor(id, color);
+    if (element) {
+        if (element.hasClass(showClass)) {
+            element.removeClass(showClass);
+        } else {
+            element.addClass(showClass);
+        }
+    
+        if (color) {
+            w3SwitchColor(id, color);
+        }
     }
 }
 
@@ -17,9 +19,11 @@ function w3SwitchColor(id, color) {
     let element = $(`#${id}`);
     let colorClass = `w3-${color}`;
 
-    if (element.hasClass(colorClass)) {
-        element.removeClass(colorClass);
-    } else {
-        element.addClass(colorClass);
+    if (element) {
+        if (element.hasClass(colorClass)) {
+            element.removeClass(colorClass);
+        } else {
+            element.addClass(colorClass);
+        }
     }
 }
